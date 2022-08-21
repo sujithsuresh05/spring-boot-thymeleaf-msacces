6e40fca300108f725b8ac782fcf66d881e2b50fb
sujith s <sujiths@sujiths-MacBook-Pro.local>
2022-08-21 10:32:00 +0530

    basic security added

---
+++ src/main/java/com/nagarro/banking/controller/BankController.java
@@ -0,0 +1,16 @@
+package com.nagarro.banking.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/bank")
+public class BankController {
+
+    @GetMapping("/dashboard")
+    public String login(Model model) {
+        return "dashboard";
+    }
+}
